1443e76357341e76f935308b35a6c3182f83fade
galamo <galamouyal88@gmail.com>
2025-07-20 11:01:19 +0300

    a

--- MFS/Lab_2/shared-lib/src/Test.tsx
+++ MFS/Lab_2/shared-lib/src/Test.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Test = () => {
   return (
     <div style={{ padding: 20, backgroundColor: "#e0e0e0" }}>
-      Hello from shared-lib!!!!!!!!!!
+      This is Shared library
     </div>
   );
 };
