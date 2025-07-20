52fddb49adde72ce44493396f256d93abd6c4dc7
galamo <galamouyal88@gmail.com>
2025-07-20 14:18:28 +0300

    a

--- MFS/Lab_3/host-client/src/App.tsx
+++ MFS/Lab_3/host-client/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Host Client </h1>
+        <h1>Host Client Application (Shell) </h1>
       </div>
       <div
         style={{
--- MFS/Lab_3/host-client/vite.config.ts
+++ MFS/Lab_3/host-client/vite.config.ts
@@ -6,14 +6,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command, mode }) => {
   const isDev = command === "serve";
   const isProd = command === "build";
-
+  const port = 5001
   return {
     plugins: [
       react(),
       federation({
         name: "host",
         remotes: {
-          flights: "http://localhost:5001/assets/remoteEntry.js"
+          flights: `http://localhost:${port}/assets/remoteEntry.js`
         },
         shared: ["@mui/material", "react", "react-dom", "@mui/x-data-grid"],
       }),
